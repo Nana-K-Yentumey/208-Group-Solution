593145795c0691b5fbe2c92445c84c5987f1a347
death-inspires-me <nana.esseney@gmail.com>
2024-06-28 19:50:10 +0000

    Finished log in pop-up, made all pages responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="nav-bar">
       <h3 className="logo-name">GRAMOPHONE GHANA</h3>
-      <button className="sign-up">SIGN UP</button>
+      <button className="sign-up"><Link to="/contactUs">SIGN UP</Link></button>
     </nav>
   );
 };
--- src/pages/LandingPage/Modal.jsx
+++ src/pages/LandingPage/Modal.jsx
@@ -1,13 +1,68 @@
 import React, { useState } from 'react';
-import './Modal.css';
-import LoginPage from './LoginPage';
+import './LandingPage.css';
 
 const Modal = ({ show, handleClose }) => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Add your login logic here
+    // Example: authenticate the user using an API call
+
+    if (username === 'admin' && password === 'password') {
+      // Successful login
+      setError('');
+      console.log('Login successful');
+      // Redirect to dashboard or another page
+    } else {
+      // Unsuccessful login
+      setError('Invalid username or password');
+    }
+  };
+
   return (
     <div className={`modal ${show ? 'show' : ''}`}>
       <div className="modal-content">
         <span className="close" onClick={handleClose}>&times;</span>
-        <LoginPage />
+        <h2>Login</h2>
+      {error && <p className="error">{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+      
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+          
+        </div>
+      </form>
+      <button type='submit' style={{ width: '100%' }}>Login</button>
       </div>
     </div>
   );
